cfa0a44ce7d08e4d34dede54e8af99411858d3c5
marcelo04 <marceloqueiroz7894@gmail.com>
2023-12-14 16:09:02 -0300

    cronometro 1.0

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Timer from './Componentes/Timer'
+
+function App() {
+
+
+  return (
+    <>
+     <Timer/>
+    </>
+  )
+}
+
+export default App
